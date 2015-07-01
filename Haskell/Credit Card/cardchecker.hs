5bde068a2e31fa71a82e79010f3455a420cc319d
Matias Grioni <matgrioni@gmail.com>
2015-07-01 14:18:32 -0400

    Luhn method finished and able to validate card numbers

--- Haskell/Credit Card/cardchecker.hs
+++ Haskell/Credit Card/cardchecker.hs
@@ -1,8 +1,5 @@
 module Main where
 
-luhn :: Integer -> Bool
-luhn n = False
-
 -- Returns a list of the digits of n in their appropriate order.
 digits :: Integer -> [Integer]
 digits n
@@ -10,13 +7,22 @@ digits n
     | n < 10    = [n]
     | otherwise = digits (n `div` 10) ++ [n `mod` 10]
 
--- Doubles every Integer in a list starting with the second.
+-- Doubles every Integer in a list starting with the first.
 -- If there are less than 2 elements in a list it returns the
 -- passed in list
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther []       = []
 doubleEveryOther (x:[])   = [x]
-doubleEveryOther (x:y:zs) = [x, 2 * y] ++ doubleEveryOther(zs)
+doubleEveryOther (x:y:zs) = [2 * x, y] ++ doubleEveryOther zs
+
+-- Sums all the digits of all entries in the provided list
+sumDigits :: [Integer] -> Integer
+sumDigits []     = 0
+sumDigits (n:ns) = sum (digits n) + sumDigits(ns)
+
+-- Determines if the provided integer is a valid credit card num
+luhn :: Integer -> Bool
+luhn n = sumDigits (doubleEveryOther (digits n)) `mod` 10 == 0
 
 main = do
-    print(doubleEveryOther (digits 1234))
+    print(luhn 1234567898765432)
