f90b1f8b44f96fa14bf907fda448e8654deef776
Matias Grioni <matgrioni@gmail.com>
2015-07-02 11:07:09 -0400

    Created count method for elements in list but still unfinished exercise

--- Haskell/Mastermind/mastermind.hs
+++ Haskell/Mastermind/mastermind.hs
@@ -16,11 +16,21 @@ data Move = Move Code Int Int
 colors :: [Peg]
 colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
+-- Counts the number of occurences of an element in a list. Define the
+-- case for no list since this last has no type technically and I'm not
+-- really sure how the type system reacts.
+count :: Eq a => [a] -> a -> Int
+count [] _ = 0
+count xs x = length (filter (\a -> a == x) xs)
+
 -- Returns the amount of exact matches between the actual code and the guess
 -- The first code is the actual code and the second one is the guess.
 exactMatches :: Code -> Code -> Int
 exactMatches actual guess = length (filter (\(a, b) -> a == b) zipped)
     where zipped = zip actual guess
 
+countColors :: Code -> [Int]
+countColors code = undefined
+
 main = do
-    print (exactMatches [Red, Red] [Blue, Red])
+    putStrLn "Hello, World!"
