570483b74bd01f4b9a5d98e77354941dd68274ae
Matias Grioni <matgrioni@gmail.com>
2015-07-26 13:56:59 -0400

    Added new poll views for viewing, results, and voting

--- Django/Poll/polls/views.py
+++ Django/Poll/polls/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     return HttpResponse("Hello, World. You're at the polls index.")
+
+def detail(request, question_id):
+    response = "You're looking at question {}"
+    return HttpResponse(response.format(question_id))
+
+def results(request, question_id):
+    response =  "You're looking at the results of question {}"
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    response = "You're voting on question {}"
+    return HttpResponse(response.format(question_id))
