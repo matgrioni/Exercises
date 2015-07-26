0dd352260a91cb7896fcd1611884ec46b60adbaf
Matias Grioni <matgrioni@gmail.com>
2015-07-26 15:19:41 -0400

    Extended views for index and detail views

--- Django/Poll/polls/views.py
+++ Django/Poll/polls/views.py
@@ -1,12 +1,23 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_questions': latest_questions }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    response = "You're looking at question {}"
-    return HttpResponse(response.format(question_id))
+    # Try to get the requested object but if it doesn't exist throw an
+    # Http404 error
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("Question does not exist")
+    
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response =  "You're looking at the results of question {}"
