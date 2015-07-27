c183f406dad2273eaeff78d6651b159f81f4c797
Matias Grioni <matgrioni@gmail.com>
2015-07-26 22:36:26 -0400

    Updated views and templates to include voting for questions

--- Django/Poll/polls/views.py
+++ Django/Poll/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
@@ -20,9 +21,22 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response =  "You're looking at the results of question {}"
-    return HttpResponse(response.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    response = "You're voting on question {}"
-    return HttpResponse(response.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html', {
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # Return a request object for posts to prevent posting data
+        # twice if back button is pressed
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
