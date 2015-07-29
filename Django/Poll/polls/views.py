1a36b38f331ebe7cda8163db3999775bbab37d7d
Matias Grioni <matgrioni@gmail.com>
2015-07-28 20:10:53 -0400

    Changed views from being method based to generic

--- Django/Poll/polls/views.py
+++ Django/Poll/polls/views.py
@@ -2,28 +2,33 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_questions': latest_questions }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    # Uses polls/index.html as the template for this generic view along with
+    # latest_question_list as the name of the variable in the template that
+    # will be listed.
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions'
 
-def detail(request, question_id):
-    # Try to get the requested object but if it doesn't exist throw an
-    # Http404 error
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("Question does not exist")
-    
-    return render(request, 'polls/detail.html', {'question': question})
+    # Returns the last 5 published questions.
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    # Expects to detail Question objects now and uses polls/details.html as the
+    # template for it.
+    model = Question
+    template_name = 'polls/detail.html'
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# Doesn't display an actual view, rather votes based on the message passed in
+# the POST request, and redirects to the results view for the question.
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
